2eeac8697ca85c619bdf6e0068d8b754f91577e6
yasuhirozenhira <poseidon.0141@gmail.com>
2019-12-21 17:20:55 +0900

    create_issue64

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -30,6 +30,8 @@ class AssignsController < ApplicationController
       I18n.t('views.messages.cannot_delete_the_leader')
     elsif Assign.where(user_id: assigned_user.id).count == 1
       I18n.t('views.messages.cannot_delete_only_a_member')
+    elsif !(assign.user_id == current_user.id || assign.team.owner == current_user )
+      I18n.t('views.messages.only the team owner or yourself can delete a user')
     elsif assign.destroy
       set_next_team(assign, assigned_user)
       I18n.t('views.messages.delete_member')
